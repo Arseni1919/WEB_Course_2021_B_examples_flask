699cb16b35ed3868781904f945bc84b98c3304af
Arseni <1919ars@gmail.com>
2021-05-23 13:43:33 +0300

    changed

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import render_template, jsonify
 from flask import session
 import mysql.connector
 
-
 app = Flask(__name__)
 app.secret_key = '123'
 
@@ -36,90 +35,116 @@ def interact_db(query, query_type: str):
     connection.close()
     cursor.close()
     return return_value
+
+
 # ------------------------------------------------- #
 # ------------------------------------------------- #
 
+
 # ------------------------------------------------- #
-# ---------------------- GET ---------------------- #
+# ------------------- SELECT ---------------------- #
 # ------------------------------------------------- #
 @app.route('/users')
 def users():
     query = "select * from users"
     query_result = interact_db(query=query, query_type='fetch')
-    print(query_result)
     return render_template('users.html', users=query_result)
+
+
 # ------------------------------------------------- #
 # ------------------------------------------------- #
 
-# ------------------------------------------------- #
-# ---------------------- GET ---------------------- #
-# ------------------------------------------------- #
-
-# @app.route('/get_user', defaults={'user_id': 15, 'product_name': 'table'})
-# @app.route('/get_user/<user_id>/product/<product_name>', methods=['GET', "POST"])
-# def get_user(user_id, product_name):
-#     if request.method == "GET":
-#         # get from DB
-#         query = "SELECT * FROM users WHERE id='%s';" % user_id
-#         query_result = interact_db(query=query, query_type='fetch')
-#         if len(query_result) == 0:
-#             return jsonify({
-#                 'success': 'False',
-#                 "data": []
-#             })
-#         else:
-#             return jsonify({
-#                 'success': 'True',
-#                 'data': query_result[0],
-#                 'product_name': product_name
-#             })
-#     if request.method == "POST":
-#         return f'The method is: {request.method}'
-#
-#
-# @app.route('/get_user_info')
-# def get_user_info():
-#     # checking
-#     print('Checking')
-#     user_id = request.args['user_id']
-#     product_name = request.args['product_name']
-#     return redirect(url_for('get_user', user_id=user_id, product_name=product_name))
-# ------------------------------------------------- #
-# ------------------------------------------------- #
 
 # ------------------------------------------------- #
 # -------------------- INSERT --------------------- #
 # ------------------------------------------------- #
+@app.route('/insert_user', methods=['GET', 'POST'])
+def insert_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        # recheck
+        query = "INSERT INTO users(name, email, password) VALUES ('%s', '%s', '%s')" % (name, email, password)
+        interact_db(query=query, query_type='commit')
+        return redirect('/users')
+    return render_template('insert_user.html', req_method=request.method)
 
-# @app.route('/insert_user', methods=['GET', 'POST'])
-# def insert_user():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         password = request.form['password']
-#         query = "INSERT INTO users(name, email, password) VALUES ('%s', '%s', '%s')" % (name, email, password)
-#         interact_db(query=query, query_type='commit')
-#         return redirect('/users')
-#     return render_template('insert-user.html', req_method=request.method)
+
 # ------------------------------------------------- #
 # ------------------------------------------------- #
+
 
 # ------------------------------------------------- #
 # -------------------- DELETE --------------------- #
 # ------------------------------------------------- #
 
-# @app.route('/delete_user', methods=['GET', 'POST'])
-# def delete_user():
-#     if request.method == 'GET':
-#         user_id = request.args['id']
-#         query = "DELETE FROM users WHERE id='%s';" % user_id
-#         interact_db(query, query_type='commit')
-#         return f'user {user_id} is deleted from db'
-#     return redirect('/users')
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    user_id = request.form['id']
+    query = "DELETE FROM users WHERE id='%s';" % user_id
+    interact_db(query, query_type='commit')
+    return redirect('/users')
+
+
+# ------------------------------------------------- #
+# ------------------------------------------------- #
+
+# ------------------------------------------------- #
+# --------------- URL PARAMETERS ------------------ #
+# ------------------------------------------------- #
+# @app.route('/profile/<int:user_id>')
+# def profile_func(user_id):
+#     return f'user ID: {user_id}'
+# ------------------------------------------------- #
+# @app.route('/profile/<int:user_id>')
+# def profile_func(user_id):
+#     return render_template('profile.html', user_id=user_id)
+# ------------------------------------------------- #
+# @app.route('/profile', defaults={'user_id': 15})
+# @app.route('/profile/<int:user_id>')
+# def profile_func(user_id):
+#     return render_template('profile.html', user_id=user_id)
+# ------------------------------------------------- #
+# @app.route('/profile', defaults={'user_id': 777, 'email': 'example@email.com'})
+# @app.route('/profile/<int:user_id>/<email>')
+# def profile_func(user_id, email):
+#     return render_template('profile.html', user_id=user_id, email=email)
+# ------------------------------------------------- #
+@app.route('/profile', defaults={'user_id': -1}, methods=["GET", "POST"])
+@app.route('/profile/<int:user_id>')
+def profile_func(user_id):
+    if user_id == -1:
+        return render_template('profile.html', fill_form=True)
+    else:
+        query = "SELECT * FROM users WHERE id='%s';" % user_id
+        query_result = interact_db(query=query, query_type='fetch')
+        if len(query_result) == 0:
+            return render_template('404.html'), 404
+            # return jsonify({
+            #                 'success': 'False',
+            #                 "data": []
+            #             })
+        else:
+            # return render_template('profile.html', user_id=user_id, query_result=query_result)
+            return jsonify({
+                'success': 'True',
+                'data': query_result[0],
+            })
+
+
+@app.route('/get_user_info', methods=['POST'])
+def get_user_info():
+    user_id = request.form['user_id']
+    return redirect(url_for('profile_func', user_id=user_id))
 # ------------------------------------------------- #
 # ------------------------------------------------- #
 
 
+# ------------------------------------------------- #
+# -------------- MULTIPLE ROUTES ------------------ #
+# ------------------------------------------------- #
+@app.route('/index')
 @app.route('/main')
 @app.route('/home')
 @app.route('/')
@@ -127,42 +152,15 @@ def index():
     # return render_template('index.html') #, name='Ariel')
     # return render_template('index.html', name=name)
     # DB
-    curr_user = {'firstname': "Ariel", 'lastname': "Perchik", 'wok': 'BGU', 'adress': 'Israel'}
     # curr_user = ''
     return render_template('index.html',
-                           curr_user=curr_user,
                            hobbies=['Prog', 'Paint', "IEM", "Swim", "Sleep"],
                            degree=('B.Sc', 'M.Sc'))
 
 
-@app.route('/customers', methods=['GET', 'POST', 'DELETE', "PUT"])
-def hello_cart():
-    # do something
-    customer_registrated = False
-    if customer_registrated:
-
-        return redirect(url_for('index'))
-    else:
-        return 'You need to LOGIN'
-
-
-@app.route('/about')
-def hello_about():
-    # do something
-    return render_template('about.html')
-
-
-@app.route('/friends')
-def hello_friends():
-    # do something
-    curr_user = {'firstname': "Ariel", 'lastname': "Perchik", 'wok': 'BGU', 'adress': 'Israel'}
-    return render_template('friends.html',
-                           curr_user = curr_user,
-                           hobbies = ['Prog', 'Paint', "IEM", "Swim", "Sleep"],
-                           degree = ('B.Sc', 'M.Sc')
-    )
-
-
+# ------------------------------------------------- #
+# --------------- MULTIPLE METHODS ---------------- #
+# ------------------------------------------------- #
 @app.route('/catalog', methods=['POST', 'PUT', 'DELETE', 'GET'])
 def catalog_func():
     curr_user = {'firstname': "Ariel", 'lastname': "Perchik", 'wok': 'BGU', 'adress': 'Israel'}
@@ -194,11 +192,55 @@ def catalog_func():
                            current_method=current_method)
 
 
+@app.route('/customers', methods=['GET', 'POST', 'DELETE', "PUT"])
+def hello_cart():
+    # do something
+    customer_registrated = False
+    if customer_registrated:
+
+        return redirect(url_for('index'))
+    else:
+        return 'You need to LOGIN'
+
+
+@app.route('/about')
+def hello_about():
+    # do something
+    return render_template('about.html')
+
+
+@app.route('/log_out')
+def log_out():
+    session['username'] = ''
+    session['lastname'] = ''
+    return redirect(url_for('index'))
+
+
+@app.route('/log_in', methods=['GET', 'POST'])
+def log_in():
+    if request.method == 'GET':
+        return render_template('log_in.html')
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        session['lastname'] = request.form['lastname']
+    return redirect(url_for('index'))
+
+
+@app.route('/friends')
+def hello_friends():
+    # do something
+    curr_user = {'firstname': "Ariel", 'lastname': "Perchik", 'wok': 'BGU', 'adress': 'Israel'}
+    return render_template('friends.html',
+                           curr_user=curr_user,
+                           hobbies=['Prog', 'Paint', "IEM", "Swim", "Sleep"],
+                           degree=('B.Sc', 'M.Sc')
+                           )
+
+
 @app.route('/menu', methods=['GET'])
 def hello_menu():
     return 'Welcome to the MENU page'
 
 
 if __name__ == '__main__':
-
     app.run(debug=True)
---
+++ print_options_example.py
@@ -0,0 +1,11 @@
+
+
+name = 'Ariel'
+second_name = 'Perchik'
+
+my_string = 'hello, %s %s!' % (name, second_name)
+
+# my_string = f'hello, {name} {second_name}!'
+
+
+print(my_string)
