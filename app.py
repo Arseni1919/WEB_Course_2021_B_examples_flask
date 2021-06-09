31a5c5b7b303e669d2ae552cbbfe525dbab19c49
Arseni <1919ars@gmail.com>
2021-06-09 11:45:20 +0300

    changed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, url_for, request
-from flask import render_template, jsonify
+from flask import render_template
 from flask import session
+from flask import jsonify
 import mysql.connector
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ def interact_db(query, query_type: str):
     #
 
     if query_type == 'commit':
-        # Use for INSERT UPDATE, DELETE statements.
+        # Use for INSERT, UPDATE, DELETE statements.
         # Returns: The number of rows affected by the query (a non-negative int).
         connection.commit()
         return_value = True
@@ -93,6 +94,54 @@ def delete_user():
 # ------------------------------------------------- #
 # --------------- URL PARAMETERS ------------------ #
 # ------------------------------------------------- #
+@app.route('/profile', defaults={'user_id': -1})
+@app.route('/profile/<int:user_id>')
+def profile_func(user_id):
+    # DB
+    if user_id == -1:
+        return render_template('profile.html', user_exists=False)
+
+    query = "SELECT * FROM users WHERE id='%s';" % user_id
+    query_result = interact_db(query=query, query_type='fetch')
+    response = {}
+    if len(query_result) != 0:
+        response = query_result[0]
+
+    response = jsonify(response)
+    return response
+
+    # return render_template('profile.html', user_id=user_id, user_exists=user_exists, category=category)
+
+
+@app.route('/profiles')
+def profiles_func():
+    user_id, category = 10, 'username'
+    # ...
+    return redirect(url_for('profile_func', user_id=user_id, category=category))
+
+#     if user_id == -1:
+#         return render_template('profile.html', fill_form=True)
+#     else:
+#         query = "SELECT * FROM users WHERE id='%s';" % user_id
+#         query_result = interact_db(query=query, query_type='fetch')
+#         if len(query_result) == 0:
+#             return render_template('404.html'), 404
+#             # return jsonify({
+#             #                 'success': 'False',
+#             #                 "data": []
+#             #             })
+#         else:
+#             # return render_template('profile.html', user_id=user_id, query_result=query_result)
+#             return jsonify({
+#                 'success': 'True',
+#                 'data': query_result[0],
+#             })
+
+
+
+
+
+
 # @app.route('/profile/<int:user_id>')
 # def profile_func(user_id):
 #     return f'user ID: {user_id}'
@@ -111,32 +160,32 @@ def delete_user():
 # def profile_func(user_id, email):
 #     return render_template('profile.html', user_id=user_id, email=email)
 # ------------------------------------------------- #
-@app.route('/profile', defaults={'user_id': -1}, methods=["GET", "POST"])
-@app.route('/profile/<int:user_id>')
-def profile_func(user_id):
-    if user_id == -1:
-        return render_template('profile.html', fill_form=True)
-    else:
-        query = "SELECT * FROM users WHERE id='%s';" % user_id
-        query_result = interact_db(query=query, query_type='fetch')
-        if len(query_result) == 0:
-            return render_template('404.html'), 404
-            # return jsonify({
-            #                 'success': 'False',
-            #                 "data": []
-            #             })
-        else:
-            # return render_template('profile.html', user_id=user_id, query_result=query_result)
-            return jsonify({
-                'success': 'True',
-                'data': query_result[0],
-            })
-
-
-@app.route('/get_user_info', methods=['POST'])
-def get_user_info():
-    user_id = request.form['user_id']
-    return redirect(url_for('profile_func', user_id=user_id))
+# @app.route('/profile', defaults={'user_id': -1}, methods=["GET", "POST"])
+# @app.route('/profile/<int:user_id>')
+# def profile_func(user_id):
+#     if user_id == -1:
+#         return render_template('profile.html', fill_form=True)
+#     else:
+#         query = "SELECT * FROM users WHERE id='%s';" % user_id
+#         query_result = interact_db(query=query, query_type='fetch')
+#         if len(query_result) == 0:
+#             return render_template('404.html'), 404
+#             # return jsonify({
+#             #                 'success': 'False',
+#             #                 "data": []
+#             #             })
+#         else:
+#             # return render_template('profile.html', user_id=user_id, query_result=query_result)
+#             return jsonify({
+#                 'success': 'True',
+#                 'data': query_result[0],
+#             })
+#
+#
+# @app.route('/get_user_info', methods=['POST'])
+# def get_user_info():
+#     user_id = request.form['user_id']
+#     return redirect(url_for('profile_func', user_id=user_id))
 # ------------------------------------------------- #
 # ------------------------------------------------- #
 
